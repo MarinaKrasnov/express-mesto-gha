d08a6a8bcae5c4507ce052abdc18685ddb050057
Marina Krasnov <marina.krasnova@gmail.com>
2022-06-28 17:21:57 +0300

    fix

--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcryptjs');
 
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
-const { NotFoundError } = require('../errors/not-found-error');
+const NotFoundError = require('../errors/not-found-error');
 
 module.exports.getUsers = (req, res) => {
   User.find()
@@ -120,14 +120,14 @@ module.exports.login = (req, res) => {
   return User.findOne({ email }).select('+password').then((user) => {
     if (!user) {
       /*    return res.status(401).send({ message: 'Пользователь не найден' }); */
-      /*   return res.status(404).send({ message: 'Пользователь не найден' }) */
-      return Promise.reject(new NotFoundError('Пользователь не найден'));
+        return res.status(404).send({ message: 'Пользователь не найден' })
+      /* return Promise.reject(new NotFoundError('Пользователь не найден')); */
     }
     return bcrypt.compare(password, user.password, ((error, isValid) => {
       if (isValid) {
         const token = jwt.sign({ id: user.id }, process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
         return res.cookie('jwt', token, { httpOnly: true, sameSite: true }).status(200).send({
-          name: user.name, about: user.about, avatar: user.avatar, email: user.email
+          name: user.name, about: user.about, avatar: user.avatar, email: user.email, _id: user.id
         });
       }
       return Promise.reject(error('Неправильные почта или пароль'));
