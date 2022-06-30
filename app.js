714d63738b9d570220ae6bd7d1efd0d540f58e4e
Marina Krasnov <marina.krasnova@gmail.com>
2022-06-30 10:18:52 +0300

    review 3

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const {
   errors, celebrate, Joi
 } = require('celebrate');
 const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
 const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const NotFoundError = require('./errors/not-found-error');
@@ -13,6 +14,8 @@ const ValidateUrl = require('./utils/constants');
 const { PORT = 3000 } = process.env;
 const app = express();
 require('dotenv').config();
+
+app.use(helmet());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
